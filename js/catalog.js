d7e64d9d3f43cfde38d3fcbb24733537474bd2a2
ArtyBorter1432 <giogio123giogiosjd@gmail.com>
2026-05-18 22:01:26 +0300

    arty

--- js/catalog.js
+++ js/catalog.js
@@ -1,27 +1,26 @@
 // Данные товаров с реальными изображениями
 const products = [
     // Смартфоны
-    { id: 1, name: "iPhone 15 Pro", price: 99990, category: "phones", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch?wid=512&hei=512&fmt=jpeg", description: "6.1″ Super Retina XDR, A17 Pro" },
-    { id: 2, name: "iPhone 15 Pro Max", price: 119990, category: "phones", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-max-finish-select-202309-6-7inch?wid=512&hei=512&fmt=jpeg", description: "6.7″ Super Retina XDR, A17 Pro" },
-    { id: 3, name: "Samsung Galaxy S24 Ultra", price: 109990, category: "phones", img: "https://image-us.samsung.com/SamsungUS/smartphones/galaxy-s24-ultra/images/gallery/s24-ultra-titanium-gray-front-back.jpg?$product-card-small-jpg$", description: "6.8″ Dynamic AMOLED, 200MP камера" },
-    { id: 4, name: "Xiaomi 14", price: 59990, category: "phones", img: "https://i01.appmifile.com/webfile/globalimg/7/8/9/789b6e3e-4d1d-4f6d-8f3e-2d5e7c8b9a0d.png", description: "6.36″ AMOLED, Snapdragon 8 Gen 3" },
+    { id: 1, name: "iPhone 15 Pro", price: 99990, category: "phones", img: "https://pcdn.goldapple.ru/p/p/19000217336/web/696d674d61696e5064708ddc56020a38c0c.jpg", description: "6.1″ Super Retina XDR, A17 Pro" },
+    { id: 2, name: "iPhone 15 Pro Max", price: 119990, category: "phones", img: "https://avatars.mds.yandex.net/get-mpic/5098271/2a00000190977ecb4078d1dcf94978fb262a/orig", description: "6.7″ Super Retina XDR, A17 Pro" },
+    { id: 3, name: "Samsung Galaxy S24 Ultra", price: 109990, category: "phones", img: "https://main-cdn.sbermegamarket.ru/big2/hlr-system/213/228/739/242/712/23/100067263729b3.webp", description: "6.8″ Dynamic AMOLED, 200MP камера" },
+    { id: 4, name: "Xiaomi 14", price: 59990, category: "phones", img: "https://avatars.mds.yandex.net/i?id=7a35afa4b75e625f041caf1468a52a93_sr-4076747-images-thumbs&n=13", description: "6.36″ AMOLED, Snapdragon 8 Gen 3" },
     
     // Ноутбуки
-    { id: 5, name: "MacBook Pro 14", price: 199990, category: "laptops", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp14-spacegray-select-202310?wid=512&hei=512&fmt=jpeg", description: "M3 Pro, 18GB RAM, 512GB SSD" },
-    { id: 6, name: "MacBook Air 13", price: 119990, category: "laptops", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mba13-midnight-select-202402?wid=512&hei=512&fmt=jpeg", description: "M3, 8GB RAM, 256GB SSD" },
-    { id: 7, name: "ASUS ROG Zephyrus", price: 149990, category: "laptops", img: "https://dlcdnwebimgs.asus.com/gain/32A6B2F2-C5A2-4F2A-8D2A-5B2A8D2F2A5C/w717", description: "Intel i9, RTX 4060, 16GB RAM" },
-    { id: 8, name: "Lenovo ThinkPad X1", price: 139990, category: "laptops", img: "https://www.lenovo.com/medias/lenovo-thinkpad-x1-carbon-gen-11-front.png?context=bWFzdGVyfHJvb3R8MjQ1NjA3fGltYWdlL3BuZ3xoMDAvaDg0LzE2MjQ5ODAyMDAwMzgucG5nfD", description: "Intel i7, 16GB RAM, 512GB SSD" },
+    { id: 5, name: "MacBook Pro 14", price: 199990, category: "laptops", img: "https://ir.ozone.ru/s3/multimedia-e/6367881386.jpg", description: "M3 Pro, 18GB RAM, 512GB SSD" },
+    { id: 6, name: "MacBook Air 13", price: 119990, category: "laptops", img: "https://avatars.mds.yandex.net/get-mpic/5332113/2a0000018eaeba54552860ab7b9b20cca5fd/orig", description: "M3, 8GB RAM, 256GB SSD" },
+    { id: 7, name: "ASUS ROG Zephyrus", price: 149990, category: "laptops", img: "https://object.pscloud.io/cms/cms/Uploads/1_DT0v5f.jpg", description: "Intel i9, RTX 4060, 16GB RAM" },
+    { id: 8, name: "Lenovo ThinkPad X1", price: 139990, category: "laptops", img: "https://cdn1.technopark.ru/technopark/photos_resized/product/1000_1000/789018/2_789018.jpg?timestamp=2025-05-12_09-21-33", description: "Intel i7, 16GB RAM, 512GB SSD" },
     
     // Аксессуары
-    { id: 9, name: "AirPods Pro 2", price: 24990, category: "accessories", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airpods-pro-2-hero?wid=512&hei=512&fmt=jpeg", description: "Активное шумоподавление" },
-    { id: 10, name: "AirPods 4", price: 14990, category: "accessories", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airpods-4-hero?wid=512&hei=512&fmt=jpeg", description: "Пространственное аудио" },
-    { id: 11, name: "Чехол для iPhone 15 Pro", price: 2990, category: "accessories", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MRWV3?wid=512&hei=512&fmt=jpeg", description: "Сilicone case, защита" },
-    { id: 12, name: "MagSafe Зарядка", price: 4990, category: "accessories", img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHXH3?wid=512&hei=512&fmt=jpeg", description: "Беспроводная зарядка 15W" },
+    { id: 9, name: "AirPods Pro 2", price: 24990, category: "accessories", img: "https://main-cdn.sbermegamarket.ru/big2/hlr-system/ccs/255258/UE9EUzJQUk9YWFhfMl82Nzg2NjUxNjk=/b3.webp", description: "Активное шумоподавление" },
+    { id: 10, name: "AirPods 4", price: 14990, category: "accessories", img: "https://main-cdn.sbermegamarket.ru/big2/hlr-system/ccs/262940/MTAwMzM2NTg5NzRfMjgwMjg1NDE=/b3.webp", description: "Пространственное аудио" },
+    { id: 11, name: "Чехол для iPhone 15 Pro", price: 2990, category: "accessories", img: "https://img.joomcdn.net/19f3329b44c33f9eafb3d69302445237cc96ecd2_original.jpeg", description: "Сilicone case, защита" },
+    { id: 12, name: "MagSafe Зарядка", price: 4990, category: "accessories", img: "https://main-cdn.sbermegamarket.ru/big1/hlr-system/926/504/099/515/123/4/600012007573b0.jpeg", description: "Беспроводная зарядка 15W" },
     
     // Аудиотехника
-    { id: 13, name: "Sony WH-1000XM5", price: 34990, category: "audio", img: "https://sony.scene7.com/is/image/sonyglobalsolutions/WH-1000XM5_Pair_B_7?$categorypdp$", description: "Наушники с шумоподавлением" },
-    { id: 14, name: "JBL Charge 5", price: 12990, category: "audio", img: "https://www.jbl.com/dw/image/v2/BFGT_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw8f8d8f8d/JBL_Charge5_Hero_Black.png", description: "Портативная колонка 20W" },
-    { id: 15, name: "Yamaha HS5", price: 24990, category: "audio", img: "https://uk.yamaha.com/en/files/HS5_19b634be0c_480x480_8c0b3f0d5c.jpg", description: "Студийные мониторы" }
+    { id: 13, name: "Sony WH-1000XM5", price: 34990, category: "audio", img: "https://indexiq.ru/storage/photo/resized/xy_1200x1200/e/tbyx0pdmwul4yx5_1495bbe.jpg.webp", description: "Наушники с шумоподавлением" },
+    { id: 14, name: "JBL Charge 5", price: 12990, category: "audio", img: "https://ir.ozone.ru/s3/multimedia-1-2/w1200/7557956390.jpg", description: "Портативная колонка 20W" },
 ];
 
 // Рендер товаров
